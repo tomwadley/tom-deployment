f8bf45f7a7b83e4057264e0a92e5463527debef6
Tom Wadley <twadley@thoughtworks.com>
2015-01-11 22:28:31 +0000

    Make sure ssh-agent is running in run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Convenience wrapper for running a playbook. For example:
 # $ ./run.sh tomwadley.net.yml
 
-ansible-playbook -i inventory --vault-password-file ~/.ansible_vault "$@"
+ssh-agent bash -c "ssh-add && ansible-playbook -i inventory --vault-password-file ~/.ansible_vault \"$@\""
