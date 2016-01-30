65f82f43a374e2db268cbfb98bb1bcbc2107c809
Tom Wadley <twadley@thoughtworks.com>
2016-01-30 00:16:26 +0000

    Enable https for saunter with Lets Encrypt and acme-tiny

---
+++ roles/acme-nginx/templates/renew_cert.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+DIR=/etc/acme/{{ site_name }}
+
+python $DIR/acme_tiny.py --account-key $DIR/account.key --csr $DIR/domain.csr --acme-dir $DIR/challenges > $DIR/signed.crt || exit 1
+
+wget -O - https://letsencrypt.org/certs/lets-encrypt-x1-cross-signed.pem > $DIR/intermediate.pem
+
+cat $DIR/signed.crt $DIR/intermediate.pem > $DIR/chained.pem
+
+service nginx reload
