e16ff92c2b465d3430c9bcbc1eb42064a798e143
Tom Wadley <twadley@thoughtworks.com>
2016-11-05 13:55:54 -0500

    Cleanup unused roles and playbooks

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Convenience wrapper for running a playbook. For example:
-# $ ./run.sh tomwadley.net.yml
-
-ssh-agent bash -c "ssh-add && ansible-playbook -i inventory --vault-password-file ~/.ansible_vault \"$@\""
