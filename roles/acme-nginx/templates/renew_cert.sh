b219a4a12a565eea60b9e9f99b4f39a28f7b0188
Tom Wadley <twadley@thoughtworks.com>
2018-09-29 00:58:35 -0500

    Move to Debian Stretch DO droplet

--- roles/acme-nginx/templates/renew_cert.sh
+++ roles/acme-nginx/templates/renew_cert.sh
@@ -2,10 +2,6 @@
 
 DIR=/etc/acme/{{ site_name }}
 
-python $DIR/acme_tiny.py --account-key $DIR/account.key --csr $DIR/domain.csr --acme-dir $DIR/challenges > $DIR/signed.crt || exit 1
-
-wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > $DIR/intermediate.pem
-
-cat $DIR/signed.crt $DIR/intermediate.pem > $DIR/chained.pem
+python $DIR/acme_tiny.py --account-key $DIR/account.key --csr $DIR/domain.csr --acme-dir $DIR/challenges > $DIR/chained.pem || exit 1
 
 service nginx reload
